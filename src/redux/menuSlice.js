66241dc93a446b922b6fffc16d2d5257c8575056
sankalp51 <sankalpkalangutkar51@gmail.com>
2024-04-09 12:34:17 +0530

    performance improvements

--- src/redux/menuSlice.js
+++ src/redux/menuSlice.js
@@ -10,19 +10,19 @@ const menuSlice = createSlice({
         filterData(state, action) {
             switch (action.payload) {
                 case "all":
-                    state.filteredItems = state.items;
+                    state.filteredItems = [...state.items];
                     break;
                 case "breakfast":
-                    state.filteredItems = state.items.filter(item => item.type === action.payload);
+                    state.filteredItems = [...state.items.filter(item => item.type === action.payload)];
                     break;
                 case "lunch":
-                    state.filteredItems = state.items.filter(item => item.type === action.payload);
+                    state.filteredItems = [...state.items.filter(item => item.type === action.payload)];
                     break;
                 case "drink":
-                    state.filteredItems = state.items.filter(item => item.type === action.payload);
+                    state.filteredItems = [...state.items.filter(item => item.type === action.payload)];
                     break;
                 default:
-                    state.filteredItems = state.items
+                    state.filteredItems = [...state.items]
             }
         }
     }
